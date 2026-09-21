f69b1dd3ea2ff20745a43015cc70b394a6b7ded8
Ujjwal Kumar <think.lab1212@gmail.com>
2026-09-21 18:17:00 +0530

    Update ProductFormPage.tsx

--- client/src/pages/admin/ProductFormPage.tsx
+++ client/src/pages/admin/ProductFormPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { useNavigate, useParams, Link } from 'react-router'
 import toast from 'react-hot-toast'
